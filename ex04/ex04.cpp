766fbbb1afadd8b73df1abb3cc68c873fac97afc
thervieu <thbault99hervieu@gmail.com>
2021-12-06 16:20:36 +0100

    tried to go too fast, and wrote cosine(ex05) over norm(ex04) | ex05 ok

--- ex04/ex04.cpp
+++ ex04/ex04.cpp
@@ -1,33 +1,26 @@
 #include "../incs/VectorSpace.hpp"
 
 int main(void) {
-	ft::VectorSpace<float> vs1({1.0, 0.0});
-	ft::VectorSpace<float> vs2({1.0, 0.0});
+	ft::VectorSpace<float> vs1({0.0, 0.0, 0.0});
     vs1.print("vs1");
-    vs2.print("vs2");
-	std::cout << "cosine: " << ft::angle_cosine(vs1, vs2) << std::endl << std::endl;
-	// 1.0
-
-	vs1 = ft::VectorSpace<float> ({1.0, 0.0});
-	vs2 = ft::VectorSpace<float> ({0.0, 1.0});
-    vs1.print("vs1");
-    vs2.print("vs2");
-	std::cout << "cosine: " << ft::angle_cosine(vs1, vs2) << std::endl << std::endl;
-	// 0.0
-
-	vs1 = ft::VectorSpace<float> ({2.0, 1.0});
-	vs2 = ft::VectorSpace<float> ({4.0, 2.0});
-    vs1.print("vs1");
-    vs2.print("vs2");
-	std::cout << "cosine: " << ft::angle_cosine(vs1, vs2) << std::endl << std::endl;
-	// 1.0
+	std::cout << "norm_1:   " << vs1.norm_1() << std::endl;
+	std::cout << "norm_e:   " << vs1.norm() << std::endl;
+	std::cout << "norm_inf: " << vs1.norm_inf() << std::endl << std::endl;
+	// 0, 0, 0
 
 	vs1 = ft::VectorSpace<float> ({1.0, 2.0, 3.0});
-	vs2 = ft::VectorSpace<float> ({4.0, 5.0, 6.0});
     vs1.print("vs1");
-    vs2.print("vs2");
-	std::cout << "cosine: " << ft::angle_cosine(vs1, vs2) << std::endl << std::endl;
-	// 0.974631846
+	std::cout << "norm_1:   " << vs1.norm_1() << std::endl;
+	std::cout << "norm_e:   " << vs1.norm() << std::endl;
+	std::cout << "norm_inf: " << vs1.norm_inf() << std::endl << std::endl;
+	// 6.0, 3.16227766, 3.0
+
+	vs1 = ft::VectorSpace<float> ({-1.0, -2.0});
+    vs1.print("vs1");
+	std::cout << "norm_1:   " << vs1.norm_1() << std::endl;
+	std::cout << "norm_e:   " << vs1.norm() << std::endl;
+	std::cout << "norm_inf: " << vs1.norm_inf() << std::endl;
+	// 3.0, 2.236067977, 2.0
 
     return (0);
 }
